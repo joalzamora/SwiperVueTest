6308b790806a5206ae0d95840010122f1270cd14
Jose Zamora <joalzamora@gmail.com>
2021-10-17 21:21:15 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+
+<template>
+<swiper :navigation="true" class="mySwiper">
+  <swiper-slide>Slide 1</swiper-slide><swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide><swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide><swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide><swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>
+</template>
+
+<script lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import "swiper/css/navigation"
+
+import './style.css';
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
+
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+        
+        
+    };
+  },
+  methods: {
+    
+  }
+  
+}
+</script>
